cf79a0aedd7c7212d0ef6977296ba5927a27d561
neyustudies <science-cat@gmx.net>
2020-06-06 17:05:15 +0200

    Create Node.hpp

---
+++ source/Node.hpp
@@ -0,0 +1,32 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+#include <iostream>
+
+
+struct Node {
+
+  int   key;      // node value
+  Node* parent;   // parent pointer
+  Node* left;     // left child pointer
+  Node* right;    // left child pointer
+
+  /* constructors for struct Node */
+
+  Node() {
+    left    = nullptr;
+    right   = nullptr;
+    parent  = nullptr;
+  }
+
+  Node(int key) {
+    this->key = key;
+    left      = nullptr;
+    right     = nullptr;
+    parent    = nullptr;
+  }
+
+};
+
+
+#endif // NODE_HPP
+
